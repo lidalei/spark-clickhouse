3db72c7b397c2d7823a22b487855ce15cd53ef42
Dalei Li <dalei.li@icloud.com>
2019-11-27 23:49:39 +0100

    add requirements.txt

---
+++ run.sh
@@ -0,0 +1,56 @@
+gcloud compute --project "youtube8m-winner" ssh --zone "europe-west4-a" "instance-1"
+
+### The following commands are run in VM
+## install docker
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# verify docker is installed successfully
+sudo docker run hello-world
+## end docker install
+
+## docker postinstall
+# https://docs.docker.com/install/linux/linux-postinstall/
+sudo groupadd docker
+sudo usermod -aG docker $USER
+exit
+### The previous commands are run in VM
+
+# restart VM
+# reconnect to VM
+gcloud compute --project "youtube8m-winner" ssh --zone "europe-west4-a" "instance-1"
+### The following commands are run in VM
+# activate the changes to groups
+newgrp docker
+# verify
+docker run hello-world
+## end docker postinstall
+
+## check if docker service starts on boot
+# systemctl list-unit-files | grep enabled
+# enable / disable this behavior
+# sudo systemctl enable docker
+# sudo systemctl disable docker
+## end check if docker service starts on boot
+### The previous commands are run in VM
+
+docker run -d --name local-clickhouse-server --ulimit nofile=262144:262144 -p 9000:9000 yandex/clickhouse-server
+# attach to the container
+docker ps -all
+# assume clickhouse container id is ec9debd359e1
+docker exec -it ec9debd359e1 /bin/bash
