52f71332b01b8e36fcd6c829e41023e0454957ac
Dalei Li <dalei.li@icloud.com>
2019-11-28 13:47:44 +0100

    install.sh docker and compose

---
+++ install.sh
@@ -0,0 +1,29 @@
+## install docker and docker compose in ubuntu
+# https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+# verify docker is installed successfully
+sudo docker run hello-world
+
+## install docker-compose 
+# https://docs.docker.com/compose/install/
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+## check if docker service starts on boot
+# systemctl list-unit-files | grep enabled
+# enable / disable this behavior
+# sudo systemctl enable docker
+# sudo systemctl disable docker
