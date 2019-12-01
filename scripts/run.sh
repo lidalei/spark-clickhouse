58218d98870db61888be964b9ba16ade5e4ec541
Dalei Li <dalei.li@icloud.com>
2019-12-02 00:00:05 +0100

    start airflow in background

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,7 @@
 pip3 install -r scripts/requirements.txt
 
-airflow initdb
-airflow webserver -p 8080
-airflow scheduler
+airflow initdb &
+airflow webserver -p 8080 &
+airflow scheduler &
 
 python3 scripts/pipeline.py
