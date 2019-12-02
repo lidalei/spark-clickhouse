3a96bead89d80c7174aae96218673ae8b52bc406
Dalei Li <dalei.li@icloud.com>
2019-12-02 11:55:00 +0100

    merge airflow pipeline, update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,34 @@
-export AIRFLOW_HOME=~/airflow
-pip3 install -r scripts/requirements.txt
+#!/bin/bash
 
-airflow initdb
-airflow webserver -p 8080 &
-airflow scheduler &
+## run spark job to download josn files and write to clickhouse	
+# export ENV variables to overwrite .env	
+# export CLICKHOUSE_PASSWORD=B1t7XFtEPGDUEIKD	
+# export ITEMS_DIR=data/items	
+# export METADATA_DIR=data/metadata	
+# verify configuration	
+docker-compose config	
+# build docker containers	
+docker-compose build	
+# create service	
+docker-compose up --no-start	
+# start clickhouse	
+docker-compose start clickhouse	
+# start spark-job	
+docker-compose start spark-job	
 
-airflow trigger_dag -sd scripts -l import-amazon-reviews
+# Stops containers and removes containers, networks, volumes, and images created by up.	
+# docker-compose down	
+
+# check runnig containers status	
+# docker-compose ps	
+# docker-compose logs SERVICE
+
+## WIP, airflow pipeline
+# export AIRFLOW_HOME=~/airflow
+# pip3 install -r scripts/requirements.txt
+
+# airflow initdb
+# airflow webserver -p 8080 &
+# airflow scheduler &
+
+# airflow trigger_dag -sd scripts -l import-amazon-reviews
